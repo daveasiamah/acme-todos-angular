9f346569d26c775f46f54b7e255e4307d90ce19a
daveasiamah <daveasiamah@gmail.com>
2021-04-19 03:37:25 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {TodoListComponent} from './todo-list/todo-list.component';
+import {TodoItemComponent} from './todo-item/todo-item.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:AppComponent},
+  {path:'todos',component:TodoListComponent},
+  {path:'todo',component:TodoItemComponent},
+  {path:'**',component:NotFoundComponent}
+  // {path:'',redirectTo:'/home',pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
